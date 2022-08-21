c89ac0a56d630f70b785b8a3f802f6794e6c091f
PeterCastillo <peterjackcc@gmail.com>
2022-08-21 00:00:07 -0500

    JS

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,11 +1,12 @@
-const XMLHttoRequest = require('xmlhttprequest').XMLHttpRequest; 
-const API =  'https://api.escuelajs.co/api/v1'
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest; 
+const API = 'https://api.escuelajs.co/api/v1'
 
 const fetchData = (urlApi, callback) =>{
-  let xhttp = new XMLHttoRequest();  
+  let xhttp = new XMLHttpRequest(); 
+
   xhttp.open('GET',urlApi,true);
   xhttp.onreadystatechange = function(event){
-    if(xhttp.readystate == 4){
+    if(xhttp.readyState == 4){
         if(xhttp.status == 200){
             callback(null, JSON.parse(xhttp.responseText));
         } else {
