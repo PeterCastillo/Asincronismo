7f5ab937120abdb74835ca4f41477122502b47b1
PeterCastillo <peterjackcc@gmail.com>
2022-08-20 23:59:17 -0500

    JS

--- src/callback/index.js
+++ src/callback/index.js
@@ -1,9 +1,25 @@
-function sum(num1,num2) {
+const sum = (num1,num2) => {
     return num1 + num2;
-};
+}
 
-function calc(num1, num2, callback){
+const calc = (num1,num2,callback) => {
     return callback(num1,num2);
-};
+}
 
 console.log(calc(2,3,sum));
+
+/*--------------------------------------------*/
+
+setTimeout( function(){
+    console.log('Hola JavaScript')
+}, 2000);
+
+/*--------------------------------------------*/
+
+const grettin = (name) => {
+    console.log(`Hola ${name}`)
+};
+
+setTimeout(grettin,4000,'Peter');
+
+/*--------------------------------------------*/
